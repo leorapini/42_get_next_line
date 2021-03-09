2710822311fc42b59545b72b04b88cbfde874597
Leo Rapini <leorapini@gmail.com>
2021-03-09 08:54:52 -0300

    restore

--- get_next_line.c
+++ get_next_line.c
@@ -12,7 +12,6 @@
 
 #include "get_next_line.h"
 
-/*
 static char			*joinstr(char const *s1, char const *s2)
 {
 	int		i;
@@ -34,30 +33,6 @@ static char			*joinstr(char const *s1, char const *s2)
 		buffer[j++] = s2[i++];
 	buffer[j] = 0;
 	return (buffer);
-}
-*/
-
-char	*joinstr(char const *s1, char const *s2)
-{
-	int		i;
-	int		len1;
-	int		len2;
-	char	*join;
-
-	if (!s1 || !s2)
-		return (NULL);
-	i = -1;
-	len1 = ft_strlen(s1);
-	len2 = ft_strlen(s2);
-	if (!(join = malloc((len1 + len2 + 1) * sizeof(*join))))
-		return (NULL);
-	while (s1[++i])
-		join[i] = s1[i];
-	i = -1;
-	while (s2[++i])
-		join[len1++] = s2[i];
-	join[len1] = '\0';
-	return (join);
 }
 
 static size_t		find_break(char *buff, char **line)
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -78,7 +78,7 @@ char			*ft_strchr(const char *s, int c)
 	return (0);
 }
 
-/*
+
 static size_t	ft_trimindex(const char *s1, const char *set, int start)
 {
 	size_t	i;
@@ -105,50 +105,8 @@ static size_t	ft_trimindex(const char *s1, const char *set, int start)
 	}
 	return (i);
 }
-*/
-static int	ft_charset(char c, char const *set)
-{
-	if (!*set)
-		return (0);
-	else if (c == *set)
-		return (1);
-	else
-		return (ft_charset(c, ++set));
-}
 
-static int	ft_left_trim(char const *s1, char const *set)
-{
-	if (!(*s1 && ft_charset(*(char *)s1, set)))
-		return (0);
-	else
-		return (ft_left_trim(++s1, set) + 1);
-}
 
-static int	ft_right_trim(char const *s1, char const *set, size_t len)
-{
-	while (s1[len] && ft_charset(s1[len], set))
-		len--;
-	return (len + 1);
-}
-
-char		*ft_strtrim(char const *s1, char const *set)
-{
-	char	*trim;
-	int		lt;
-	int		rt;
-
-	if (!s1 || !set)
-		return (NULL);
-	lt = ft_left_trim(s1, set);
-	rt = ft_right_trim(s1, set, ft_strlen(s1) - 1);
-	rt < lt ? rt = lt : rt;
-	if (!(trim = malloc((rt - lt + 1) * sizeof(*trim))))
-		return (NULL);
-	ft_strlcpy(trim, &s1[lt], rt - lt + 1);
-	return (trim);
-}
-
-/*
 char			*ft_strtrim(char const *s1, char const *set)
 {
 	size_t	start_i;
@@ -171,4 +129,4 @@ char			*ft_strtrim(char const *s1, char const *set)
 	ft_strlcpy(buffer, s1 + start_i, end_i - start_i + 2);
 	return (buffer);
 }
-*/
+
