f4ed0ce39ed285465a86db09336cc53b5dc71049
Leo Rapini <leorapini@gmail.com>
2021-05-18 14:40:50 -0300

    file restructure

--- compile.sh
+++
@@ -1,7 +0,0 @@
-gcc -Wall -Werror -Wextra -g -D BUFFER_SIZE=1 main.c get_next_line.c get_next_line_utils.c
-echo "BUFFER 1"
-./a.out ./test/plano_de_carreira.txt
-
-gcc -Wall -Werror -Wextra -g -D BUFFER_SIZE=200 main.c get_next_line.c get_next_line_utils.c
-echo "BUFFER 200"
-./a.out ./test/plano_de_carreira.txt
